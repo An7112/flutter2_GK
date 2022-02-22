024bd93b51b8c276d0444a6197608fc7b21cd57e
An7112 <thanhan7112@gmail.com>
2022-02-22 22:08:23 +0700

    Reacting to a push notification and Handling background notifications

--- flutter2_gk/lib/main.dart
+++ flutter2_gk/lib/main.dart
@@ -2,6 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter2_gk/model/PushNotification.dart';
+import 'package:overlay_support/overlay_support.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() {
   runApp(MyApp());
@@ -10,14 +15,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return OverlaySupport(
+        child: MaterialApp(
       title: 'Notify',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
+    ));
   }
 }
 
@@ -33,7 +39,18 @@ class _HomePageState extends State {
   @override
   void initState() {
     _totalNotifications = 0;
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      PushNotification notification = PushNotification(
+        title: message.notification?.title,
+        body: message.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    });
     super.initState();
+    checkForInitialMessage();
   }
 
   void registerNotification() async {
@@ -42,7 +59,7 @@ class _HomePageState extends State {
 
     // 2. Instantiate Firebase Messaging
     _messaging = FirebaseMessaging.instance;
-
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
     // 3. On iOS, this helps to take the user permissions
     NotificationSettings settings = await _messaging.requestPermission(
       alert: true,
@@ -55,6 +72,16 @@ class _HomePageState extends State {
       print('User granted permission');
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         // Parse the message received
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
         PushNotification notification = PushNotification(
           title: message.notification?.title,
           body: message.notification?.body,
@@ -67,6 +94,23 @@ class _HomePageState extends State {
       });
     } else {
       print('User declined or has not accepted permission');
+    }
+  }
+
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      PushNotification notification = PushNotification(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
     }
   }
 
@@ -91,7 +135,28 @@ class _HomePageState extends State {
           SizedBox(height: 16.0),
           NotificationBadge(totalNotifications: _totalNotifications),
           SizedBox(height: 16.0),
-          // TODO: add the notification text here
+          _notificationInfo != null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITLE: ${_notificationInfo!.title}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'BODY: ${_notificationInfo!.body}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
         ],
       ),
     );
